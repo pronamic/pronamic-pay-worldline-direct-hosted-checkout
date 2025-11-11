ffca527bc3ec15abcad0ab499109fd5b50391a1f
Remco Tolsma <info@remcotolsma.nl>
2025-11-11 11:43:47 +0100

    Update rector.php

--- rector.php
+++ rector.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
-    ->withPaths([
-        __DIR__ . '/src',
-    ])
-    // uncomment to reach your current PHP version
-    // ->withPhpSets()
-    ->withTypeCoverageLevel(0)
-    ->withDeadCodeLevel(0)
-    ->withCodeQualityLevel(0);
+	->withPaths(
+		[
+			__DIR__ . '/src',
+		]
+	)
+	->withPhpSets()
+	->withTypeCoverageLevel( 0 )
+	->withDeadCodeLevel( 0 )
+	->withCodeQualityLevel( 0 );
